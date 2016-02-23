1ef2d72d40ef2b6b5c2083555d5fa76c10a23c98
LuisBoyokan <luis.loyola.boyokan@gmail.com>
2016-02-23 01:50:53 -0300

    ADD: PautaRadioButtonHandler

--- app/src/main/java/teachapps/gocheck/CrearPautaActivity.java
+++ app/src/main/java/teachapps/gocheck/CrearPautaActivity.java
@@ -2,7 +2,12 @@ package teachapps.gocheck;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Luis on 18/02/2016.
@@ -10,8 +15,15 @@ import android.widget.TextView;
 public class CrearPautaActivity extends Activity {
 
     private static final String TAG = "CrearPauta";
+    private static final int numberOfRows = 20;
+    private static final int numberOfAlternatives = 4;
 
     private TextView mTextViewPauta;
+    private EditText mEditTextNumeroPreguntas;
+    private Button mButtonAgregarPreguntas;
+    private Button mButtonGuardarPauta;
+    private PautaRadioButtonHandler radioButtonHandler;
+
     private Long EvaluacionID;
 
     @Override
@@ -23,14 +35,49 @@ public class CrearPautaActivity extends Activity {
         //Obtener datos del activity anterior
         Bundle bundleRecibido = getIntent().getExtras();
         EvaluacionID = bundleRecibido.getLong("EvaluacionID");
-        mTextViewPauta.setText("Evaluación recien creada: " + EvaluacionID.toString());
+
     }
 
     private void initCrearPauta() {
-        //TODO completar esto
-
         //TextViews
         this.mTextViewPauta             = (TextView) findViewById(R.id.textview_pauta);
+
+        //EditTexts
+        this.mEditTextNumeroPreguntas   = (EditText) findViewById(R.id.text_cantidad_preguntas);
+
+        //Buttons
+        this.mButtonAgregarPreguntas    = (Button) findViewById(R.id.button_agregar_preguntas);
+        this.mButtonGuardarPauta        = (Button) findViewById(R.id.button_guardarPauta);
+
+        //PautaRadioButtons in RadioButtonHandler
+        this.radioButtonHandler = new PautaRadioButtonHandler(this, numberOfRows, numberOfAlternatives);
+
+        //Set Buttons' OnClickListener
+        this.mButtonAgregarPreguntas.setOnClickListener(new onClickAgregarPreguntaButton());
+        this.mButtonGuardarPauta.setOnClickListener(new onClickGuardarPautaButton());
+    }
+
+    class onClickAgregarPreguntaButton implements View.OnClickListener{
+        onClickAgregarPreguntaButton(){
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(CrearPautaActivity.this, "Funcionalidad aun no disponible", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    class onClickGuardarPautaButton implements View.OnClickListener{
+        onClickGuardarPautaButton(){
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            //TODO guardar pauta en DB
+            Toast.makeText(CrearPautaActivity.this, "Guardando", Toast.LENGTH_LONG).show();
+        }
     }
 
 
